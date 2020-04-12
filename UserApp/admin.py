1428c7f911976284039e0803cf868ef45b8d2e63
Jaber Al Siam <jaberalsiam@gmail.com>
2020-04-13 04:23:27 +0600

    Login/Signup/Profile updated

--- UserApp/admin.py
+++ UserApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SignUPTable
+from .models import SignUPTable, ProfileTable
 
 # Register your models here.
 
 admin.site.register(SignUPTable)
+admin.site.register(ProfileTable)
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,24 +1,47 @@
-from django.shortcuts import render
-from .models import SignUPTable
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 
 # Create your views here.
 
+# def signup(request):
+#     obj = SignUPTable.objects.get(id=1)
+#
+#     context = {
+#         "data": "obj"
+#     }
+#
+#     return render(request, 'user/signup.html', context)
+
+
 def signup(request):
-    '''obj = SignUPTable.objects.get(id=1)'''
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            messages.success(request, f'Your account created successfully {username}')
+            login(request, user)
+            return redirect('Home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
-    context = {
-        "data": "obj"
-    }
 
-    return render(request, 'user/signup.html', context)
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
 
-
-def login(request):
-    '''obj = SignUPTable.objects.get(id=1)'''
-
-    context = {
-        "data": "obj"
-    }
-
-    return render(request, 'user/login.html', context)
+# def login(request):
+#     obj = SignUPTable.objects.get(id=1)
+#
+#     context = {
+#         "data": "obj"
+#     }
+#
+#     return render(request, 'user/login.html', context)
